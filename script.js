6e22061535bd4f1fd20bd8c333e259907b60f3db
komala1219 <123307320+komala1219@users.noreply.github.com>
2023-04-11 07:49:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function firstNonRepeatedCharacter(str) {
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (str.indexOf(char) === i && str.indexOf(char, i + 1) === -1) {
+      return char;
+    }
+  }
+  return null;
+}
+
+const inputStr = prompt("Enter a string");
+const result = firstNonRepeatedCharacter(inputStr);
+alert(result ? `The first non-repeated character is: ${result}` : "All characters are repeated");
