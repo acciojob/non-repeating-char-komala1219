dfab659073d0c923ca6a0422b14e74b317258d16
komala1219 <123307320+komala1219@users.noreply.github.com>
2023-04-14 15:18:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 //your JS code here. If required.
 function firstNonRepeatedCharacter(str) {
+  et charCount = {};
+
+  // Loop through the string and populate the charCount object
   for (let i = 0; i < str.length; i++) {
     let char = str[i];
-    if (str.indexOf(char) === i && str.indexOf(char, i + 1) === -1) {
+    charCount[char] = charCount[char] ? charCount[char] + 1 : 1;
+  }
+
+  // Loop through the string again to find the first non-repeated character
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (charCount[char] === 1) {
       return char;
     }
   }
+
+  // Return null if no non-repeated character is found
   return null;
 }
 
